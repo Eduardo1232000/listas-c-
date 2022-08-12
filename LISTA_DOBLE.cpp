db19a6f5186dc18fb4e8d5945571273d805bfdfa
Eduardo Reyes <eduardoalex2000@hotmail.com>
2022-08-12 16:17:13 -0600

    [ADD] LISTA DOBLE y funcion de mostrar elemento inicial y final

--- LISTA_DOBLE.cpp
+++ LISTA_DOBLE.cpp
@@ -5,28 +5,30 @@ using namespace std;
 class Lista{
 private:
 public:
-    Nodo_simple *inicio;
-    Nodo_simple *final;
-    Nodo_simple *actual;
+    Nodo_doble *inicio;
+    Nodo_doble *final;
+    Nodo_doble *actual;
 
     void agregar(int dato){
-        Nodo_simple* nuevo = new Nodo_simple(dato);
+        Nodo_doble* nuevo = new Nodo_doble(dato);
         nuevo->dato = dato;
         if (this->inicio == NULL){
-            this->inicio = new Nodo_simple(dato);
-            this->final = new Nodo_simple(dato);
+            this->inicio = new Nodo_doble(dato);
+            this->final = new Nodo_doble(dato);
         }else{
             actual = inicio;
             while(actual->siguiente!=NULL){
                 actual= actual->siguiente;
             }
+            
             actual->siguiente= nuevo;
+            nuevo->anterior= final;
             this->final = nuevo;
         }
     }
 
     void mostrar(){
-        Nodo_simple* actual = this->inicio;
+        Nodo_doble* actual = this->inicio;
         if(actual!=NULL){
             cout<<"[";
             while(actual != NULL){
@@ -43,19 +45,20 @@ public:
             cout<<"Vacio";
         }
     }
-        void mostrar_inicial(){
-        Nodo_simple* actual = this->inicio;
+
+    void mostrar_inicial(){
+        Nodo_doble* actual = this->inicio;
         cout<<actual->dato<<endl;
     }
 
     void mostrar_final(){
-        Nodo_simple* actual = this->final;
+        Nodo_doble* actual = this->final;
         cout<<actual->dato<<endl;
     }
 
 };
 
-
+    
 //pruebas
 int main() {
     //a partir de aqui se puede escribir codigo"
@@ -72,13 +75,16 @@ int main() {
     lista1->agregar(5);
     lista1->agregar(4);
     lista1->agregar(3);
+    lista1->agregar(2);
+
     cout<<"Lista 2: ";
     lista1->mostrar();
 
     cout<<"ELEMENTO INICIAL:  ";
-    l->mostrar_inicial();
+    lista1->mostrar_inicial();
     
     cout<<"ELEMENTO FINAL:  ";
-    l->mostrar_final();
+    lista1->mostrar_final();
+
     return 0;
 }
--- NODO.cpp
+++ NODO.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 using namespace std;
 
-class Nodo
-{
+class Nodo_simple{
 public:
     int dato;
-    Nodo *siguiente;
-    Nodo(int dato)
+    Nodo_simple *siguiente;
+    Nodo_simple(int dato)
+    {
+        this->dato = dato;
+        this->siguiente = NULL;
+    }
+};
+
+class Nodo_doble{
+public:
+    int dato;
+    Nodo_doble *siguiente;
+    Nodo_doble *anterior;
+    Nodo_doble(int dato)
     {
         this->dato = dato;
         this->siguiente = NULL;
